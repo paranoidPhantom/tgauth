02aa0c0e6439b4b68b915207204830362ce0547b
Andrei <andrei.hudalla@outlook.com>
2024-06-18 21:20:06 -0500

    chore: build a small demo for the module

--- playground/app.vue
+++ playground/app.vue
@@ -1,8 +1,25 @@
+<script lang="ts" setup>
+import { UAccordion } from "#components";
+
+const tguser = useTelegramUser();
+
+const list = [
+    {
+        label: "User data",
+        slot: "data",
+    },
+];
+</script>
+
 <template>
-  <div>
-    Nuxt module playground!
-  </div>
+    <div class="__demo p-8 flex flex-col items-center gap-8">
+        <TelegramAuth bot="tbc_1_bot" />
+        <UAccordion :items="list">
+            <template #data>
+                <pre v-if="tguser">{{ tguser }}</pre>
+            </template>
+        </UAccordion>
+    </div>
 </template>
 
-<script setup>
-</script>
+<script setup></script>
