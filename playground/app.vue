76e76e0e790e51ca8a4a7605e3d463d82b51f92d
Andrei <andrei.hudalla@outlook.com>
2024-06-19 20:02:57 -0500

    eslint

--- playground/app.vue
+++ playground/app.vue
@@ -1,25 +1,25 @@
 <script lang="ts" setup>
-import { UAccordion } from '#components'
+import { UAccordion } from "#components";
 
-const tguser = useTelegramUser()
+const tguser = useTelegramUser();
 
 const list = [
-  {
-    label: 'User data',
-    slot: 'data',
-  },
-]
+	{
+		label: "User data",
+		slot: "data",
+	},
+];
 </script>
 
 <template>
-  <div class="__demo p-8 flex flex-col items-center gap-8">
-    <TelegramAuth bot="tbc_1_bot" />
-    <UAccordion :items="list">
-      <template #data>
-        <pre v-if="tguser">{{ tguser }}</pre>
-      </template>
-    </UAccordion>
-  </div>
+	<div class="__demo p-8 flex flex-col items-center gap-8">
+		<TelegramAuth bot="tbc_1_bot" />
+		<UAccordion :items="list">
+			<template #data>
+				<pre v-if="tguser">{{ tguser }}</pre>
+			</template>
+		</UAccordion>
+	</div>
 </template>
 
 <script setup></script>
--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,9 +1,9 @@
 export default defineNuxtConfig({
-  modules: ['../src/module', '@nuxt/ui'],
-  tgauth: {},
-  devServer: {
-    port: 80,
-    host: '0.0.0.0',
-  },
-  devtools: { enabled: true },
-})
+	modules: ["../src/module", "@nuxt/ui"],
+	tgauth: {},
+	devServer: {
+		port: 80,
+		host: "0.0.0.0",
+	},
+	devtools: { enabled: true },
+});
