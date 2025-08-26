e0d49a6882ff4f504ffefbee2823d6398b0de12d
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2025-08-25 22:49:09 -0500

    style: eslint

--- src/module.ts
+++ src/module.ts
@@ -18,10 +18,10 @@ export default defineNuxtModule<ModuleOptions>({
 	},
 	setup(_options, _nuxt) {
 		const resolver = createResolver(import.meta.url);
-		const token =
-			process.env.TG_BOT_TOKEN ??
-			_options.token ??
-			(_nuxt.options.runtimeConfig.tgauth as ModuleOptions)?.token;
+		const token
+			= process.env.TG_BOT_TOKEN
+			?? _options.token
+			?? (_nuxt.options.runtimeConfig.tgauth as ModuleOptions)?.token;
 		if (token) {
 			_nuxt.options.runtimeConfig.tgauth = defu(
 				_nuxt.options.runtimeConfig.tgauth as ModuleOptions,
@@ -30,9 +30,9 @@ export default defineNuxtModule<ModuleOptions>({
 				},
 			);
 		}
-		const expiration =
-			_options.auth_expiration ??
-			(_nuxt.options.runtimeConfig.tgauth as ModuleOptions).auth_expiration;
+		const expiration
+			= _options.auth_expiration
+			?? (_nuxt.options.runtimeConfig.tgauth as ModuleOptions).auth_expiration;
 		if (expiration)
 			_nuxt.options.runtimeConfig.tgauth = defu(
 				_nuxt.options.runtimeConfig.tgauth as ModuleOptions,
